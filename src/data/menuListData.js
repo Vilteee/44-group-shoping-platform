332268cb5965c00a1a0d3bcd69bd744209c9361f
PavasarioSniegas <vilte.kar@gmail.com>
2023-07-26 23:34:48 +0300

    Add data files

---
+++ src/data/menuListData.js
@@ -0,0 +1,11 @@
+export const menuList= [
+    {buttonName: 'Product catalog', link: "product-catalog"},
+    {buttonName: 'Shops', link: 'shops'},
+    {buttonName: 'Shopping carts', link: 'shopping-carts'},
+    {buttonName: 'New shopping cart', link:'new-shopping-cart' },
+    {buttonName: 'New product', link: 'new-product'},
+    {buttonName: 'New shop', link: 'new-shop'},
+    {buttonName: 'Account', link: 'account'},
+    {buttonName: 'Settings', link: 'settings'},
+    {buttonName: 'Sign out', link: 'sign-out'}
+]
--- src/data/registrationAndLoginData.js
+++ src/data/registrationAndLoginData.js
@@ -1,15 +1,15 @@
 
  export const registrationAndLoginData = [
     {
-      id: 'login',
-      titleOfPage: 'Create Your account',
-      button1: 'Register',
-      button2: 'Login'
+      id: '/login',
+      titleOfPage: 'Log in to your account',
+      button1: 'Login',
+      button2: 'Register',
     },
     {
-      id: 'registration',
-      titleOfPage: 'Log in to your account',
-      button1: 'Log in',
-      button2: 'Register'
+      id: '/registration',
+      titleOfPage: 'Create Your account',
+      button1: 'Register',
+      button2: 'Log in',
     }
   ]
