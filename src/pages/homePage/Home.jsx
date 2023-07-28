fd45efbdad2ba5197c0b25a1ac1cb745d4fd5fee
PavasarioSniegas <vilte.kar@gmail.com>
2023-07-28 14:14:06 +0300

    Navigate to account if already logged in in homepage

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,16 +1,27 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import style from './HomePage.module.css';
 import style1 from '../../components/button/Button.module.css'
 import { GiWaveSurfer } from 'react-icons/gi';
 
 export function Home() {
+    const navigate = useNavigate();
+
+    const handleGetStarted = () => {
+        const token = JSON.parse(localStorage.getItem('token'));
+        if(token){
+            navigate('/account');
+        } else {
+            navigate('/feature/1')
+        }
+    }
+
     return (
         <div className={style.homePage}>
             <div className={style.homePageLogoPlace}>
                 <GiWaveSurfer className={style.homePageLogo} />
             </div>
             <h1 className={style.homePageTitle}>Surfer needs</h1>
-            <Link to='feature/1' className={style1.button}>Get started</Link>
+            <button className={style1.button} onClick={handleGetStarted} >Get started</button>
         </div>
     )
 }
