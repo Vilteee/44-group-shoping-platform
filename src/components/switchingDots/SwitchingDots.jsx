16e6d4852fc532eb12d4b6404c8f5f63c775ba4a
PavasarioSniegas <vilte.kar@gmail.com>
2023-07-28 12:57:42 +0300

    Fix circle styling in switching dots

--- src/components/switchingDots/SwitchingDots.jsx
+++ src/components/switchingDots/SwitchingDots.jsx
@@ -1,20 +1,18 @@
 import style from './SwitchingDots.module.css';
-import { GoDot } from 'react-icons/go';
+import { GoDot, GoDotFill } from 'react-icons/go';
 
 
 export function SwitchingDots({index}) {
 
-    const getStyle = (number) => {
-        const colorClass = index === number ? style.dotWithColor : '';
-        
-        return `${style.switchingDots} ${colorClass}`;
+    const getBubble = (number) => {
+        return index !== '' + number ? <GoDot className={style.switchingDots}/> : <GoDotFill className={style.dotWithColor}/>;
     }
 
     return (
         <div className={style.switchingDotsPlace}>
-            <GoDot className={getStyle(1)}/>
-            <GoDot className={getStyle(2)}/>
-            <GoDot className={getStyle(3)}/>
+            {getBubble(1)}
+            {getBubble(2)}
+            {getBubble(3)}
         </div>
     )
 }
