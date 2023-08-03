f56bf1ac4a88a01c36814a19983c298d9b6d5683
PavasarioSniegas <vilte.kar@gmail.com>
2023-08-04 00:12:09 +0300

    menu burger buttons fifing

--- src/components/burgerButtonClosing/BurgerButtonClosing.jsx
+++ src/components/burgerButtonClosing/BurgerButtonClosing.jsx
@@ -1,11 +1,13 @@
 import style from './BurgerButtonClosing.module.css';
 
 
-export function BurgerButtonClosing() {
+export function BurgerButtonClosing({update}) {
+    
     return (
-        <div className={style.burgerButtonClosing}>
+        <div onClick={update} className={style.burgerButtonClosing}>
             <div className={style.line} id={style.line1}></div>
             <div className={style.line} id={style.line2}></div>
         </div>
     )
 }
+
